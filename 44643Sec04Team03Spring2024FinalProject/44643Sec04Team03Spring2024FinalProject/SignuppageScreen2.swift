06cead6efb2e0ff2d98d507b70b9e6b08b44d383
TejeswarBobba <Tejeswarbobba04@gmail.com>
2024-03-07 21:20:00 -0600

    Cocoa touch class has been created and updated actions on it.

---
+++ 44643Sec04Team03Spring2024FinalProject/44643Sec04Team03Spring2024FinalProject/SignuppageScreen2.swift
@@ -0,0 +1,29 @@
+//
+//  SignuppageScreen2.swift
+//  44643Sec04Team03Spring2024FinalProject
+//
+//  Created by Tejeswar Bobba on 3/7/24.
+//
+
+import UIKit
+
+class SignuppageScreen2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
