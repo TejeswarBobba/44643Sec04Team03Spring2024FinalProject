6187c1291a76d7722b825bdb5ccad424323b2f17
TejeswarBobba <Tejeswarbobba04@gmail.com>
2024-03-27 08:12:30 -0500

    created viewcontroller and cocoatouch class

---
+++ 44643Sec04Team03Spring2024FinalProject/44643Sec04Team03Spring2024FinalProject/DoctorLoginPage.swift
@@ -0,0 +1,29 @@
+//
+//  DoctorLoginPage.swift
+//  44643Sec04Team03Spring2024FinalProject
+//
+//  Created by Tejeswar Bobba on 3/27/24.
+//
+
+import UIKit
+
+class DoctorLoginPage: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
